5de8c5c42c33170c31028d4d95972fa778cd20f9
Nebby <nebby131999@gmail.com>
2023-02-22 13:06:38 -0300

    nuky

---
+++ SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/Nuke.cs
@@ -0,0 +1,16 @@
+﻿using RoR2;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonstorm.Starstorm2.Survivors
+{
+    public class Nuke : SurvivorBase
+    {
+        public override SurvivorDef SurvivorDef => SS2Assets.LoadAsset<SurvivorDef>("Nuke", SS2Bundle.Indev);
+
+        public override GameObject BodyPrefab => SS2Assets.LoadAsset<GameObject>("NukeBody", SS2Bundle.Indev);
+
+        public override GameObject MasterPrefab => null;
+    }
+}
